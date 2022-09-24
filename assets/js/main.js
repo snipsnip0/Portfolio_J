cb44ea7c0a13954f06f5ad236a6adb16a99b28f9
Bruno Dias <brunodias.snips@gmail.com>
2022-09-24 17:34:22 +0100

    mobile-landing

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ fetch(`https://api.github.com/users/JasminiSantos/repos`)
 
             let project_language = document.createElement('span');
             project_language.classList.add('tag', 'tag-teal');
-            project_language.textContent = projeto["language"];
+            project_language.textContent = projeto["language"] ? projeto["language"] : 'Other';
 
             let project_description = document.createElement('p');
             project_description.textContent = (projeto["description"]) ? projeto["description"] : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
